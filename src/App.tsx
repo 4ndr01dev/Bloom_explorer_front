f0bf73bcfb2dbd2f2ae49551bfc64a84a79fce65
Alvaro Andrade Ardiles <dremian42@gmail.com>
2024-04-20 10:21:39 -0400

    feat 🔧  (prettierrc): - lint config Changes to be committed: 	new file:   .eslintrc.json 	new file:   .prettierrc 	modified:   package.json 	new file:   pnpm-lock.yaml 	modified:   src/App.tsx 	modified:   src/index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello World
-        </p>
+        <p>Hello World</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
