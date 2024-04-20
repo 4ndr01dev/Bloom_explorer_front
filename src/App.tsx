f3694ebcabe75c463516fd6e21bd182d0a513995
Alvaro Andrade Ardiles <dremian42@gmail.com>
2024-04-20 11:08:43 -0400

    feat ✨ 💄 🔧   (molecules/Navbar): - navbar -mixins -svgconfig Changes to be committed: 	modified:   src/App.tsx 	new file:   src/Components/molecules/Navbar.tsx 	new file:   src/Components/molecules/navbar.scss 	new file:   src/assets/logo_small.svg 	new file:   src/custom.d.ts 	new file:   src/styles/mixins/mixins.scss 	modified:   tsconfig.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.scss'
+import Navbar from './Components/molecules/Navbar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar/>
         <p>Hello World</p>
       </header>
     </div>
